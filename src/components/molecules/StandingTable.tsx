eccd1f56452bf30aaa8252b392118f6ed69a91b7
Colt45s <kzk0693@gmail.com>
2018-10-19 02:01:33 +0900

    add column

--- src/components/molecules/StandingTable.tsx
+++ src/components/molecules/StandingTable.tsx
@@ -1,27 +1,54 @@
 import * as React from 'react'
 import { TeamRecord } from '../../models/division'
-import { Table } from 'semantic-ui-react'
+import { Table, Label } from 'semantic-ui-react'
+
+const parseClinchIndicator = (ci: string): string => {
+  const ciType: { [clinchIndicator: string]: string } = {
+    w: 'wild card',
+    y: 'division champ',
+    z: 'best record in league'
+  }
+
+  return ciType[ci]
+}
 
 const StandingTable = ({ teamRecords }: { teamRecords: TeamRecord[] }) => {
   return (
-    <Table celled={true}>
+    <Table inverted={false}>
       <Table.Header>
         <Table.Row>
           <Table.HeaderCell>Teams</Table.HeaderCell>
           <Table.HeaderCell>Wins</Table.HeaderCell>
           <Table.HeaderCell>Losses</Table.HeaderCell>
           <Table.HeaderCell>Pct</Table.HeaderCell>
+          <Table.HeaderCell>GB</Table.HeaderCell>
+          <Table.HeaderCell>WCGB</Table.HeaderCell>
+          <Table.HeaderCell>E#</Table.HeaderCell>
+          <Table.HeaderCell>STRK</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
 
       <Table.Body>
-        {teamRecords.map(team => {
+        {teamRecords.map(r => {
           return (
-            <Table.Row key={team.team.id}>
-              <Table.Cell>{team.team.name}</Table.Cell>
-              <Table.Cell>{team.wins}</Table.Cell>
-              <Table.Cell>{team.losses}</Table.Cell>
-              <Table.Cell>{team.leagueRecord.pct}</Table.Cell>
+            <Table.Row key={r.team.id}>
+              <Table.Cell>
+                {r.team.name}
+                {r.clinchIndicator ? (
+                  <Label horizontal={true} style={{ marginLeft: '10px' }}>
+                    {parseClinchIndicator(r.clinchIndicator)}
+                  </Label>
+                ) : (
+                  ''
+                )}
+              </Table.Cell>
+              <Table.Cell>{r.wins}</Table.Cell>
+              <Table.Cell>{r.losses}</Table.Cell>
+              <Table.Cell>{r.leagueRecord.pct}</Table.Cell>
+              <Table.Cell>{r.gameBack}</Table.Cell>
+              <Table.Cell>{r.wildCardGamesBack}</Table.Cell>
+              <Table.Cell>{r.eliminationNumber}</Table.Cell>
+              <Table.Cell>{r.streak.streakCode}</Table.Cell>
             </Table.Row>
           )
         })}
