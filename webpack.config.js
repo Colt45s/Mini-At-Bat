45b86471a09af68afcd22965ea3be66a8dab4797
Colt45s <kzk0693@gmail.com>
2018-10-18 18:55:19 +0900

    add config

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const webpack = require("webpack");
+const path = require("path");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const MODE = process.env.NODE_ENV || "development";
+const IS_DEV_MODE = MODE === "development";
+const DEV_PORT = 8080;
+
+const config = {
+  mode: MODE,
+  entry: {
+    main: "./src/main.tsx"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(MODE)
+      }
+    })
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [{
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "awesome-typescript-loader",
+          options: {
+            configFileName: "./tsconfig.json"
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  }
+};
+
+const devConfig = {
+  // entry: {
+  //   "hot-loader": [
+  //     "react-hot-loader/patch",
+  //     `webpack-dev-server/client?http://localhost:${DEV_PORT}`,
+  //     "webpack/hot/only-dev-server"
+  //   ]
+  // },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    // hot: true,
+    port: DEV_PORT,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+    // ,new webpack.HotModuleReplacementPlugin()
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: false
+      })
+    ]
+  }
+};
+
+const prdConfig = {};
+
+module.exports = merge(config, IS_DEV_MODE ? devConfig : prdConfig);
