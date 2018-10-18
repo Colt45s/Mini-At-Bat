c7b0b0886dc8986faa32818af6d4e04b5c90c3cb
Colt45s <kzk0693@gmail.com>
2018-10-19 02:01:01 +0900

    change design tab & add loading

--- src/components/atoms/TabContent.tsx
+++ src/components/atoms/TabContent.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
 import { Tab } from 'semantic-ui-react'
 
-export default (props: any) => <Tab.Pane>{props.children}</Tab.Pane>
+export default (props: any) => (
+  <Tab.Pane attached={false}>{props.children}</Tab.Pane>
+)
--- src/components/molecules/DivisionTab.tsx
+++ src/components/molecules/DivisionTab.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import { Division } from '../../models/division'
 import StandingTable from './StandingTable'
-import { Tab } from 'semantic-ui-react'
+import { Tab, Loader, Header } from 'semantic-ui-react'
 import TabContent from '../atoms/TabContent'
+import Overlay from '../atoms/Overlay'
 
 function createPanes(divisions: Division[]) {
   return divisions.map(d => ({
     menuItem: getMatchDivisionName(d.division.id),
     render: () => (
-      <TabContent>
+      <TabContent key={d.division.id}>
         <StandingTable teamRecords={d.teamRecords} />
       </TabContent>
     )
@@ -30,12 +31,25 @@ function getMatchDivisionName(id: number) {
   return divisions[id]
 }
 
-const DivisionTab = ({ divisions }: { divisions: Division[] }) => {
-  if (divisions.length) {
-    return <Tab panes={createPanes(divisions)} />
-  } else {
-    return <div />
+const DivisionTab = ({
+  divisions,
+  isLoading
+}: {
+  divisions: Division[]
+  isLoading: boolean
+}) => {
+  if (isLoading) {
+    return <Overlay children={<Loader>Loading</Loader>} />
   }
+  return (
+    <>
+      <Header as="h1">Standing</Header>
+      <Tab
+        menu={{ attached: false, tabular: false }}
+        panes={createPanes(divisions)}
+      />
+    </>
+  )
 }
 
 export default DivisionTab
