d8716f520a475af0b90cc8316d49e60ade24d2ed
Colt45s <kzk0693@gmail.com>
2018-10-20 03:55:54 +0900

    move constants

---
+++ src/utils/mlbConstants.ts
@@ -0,0 +1,16 @@
+export const divisionNames: {
+  [id: number]: string
+} = {
+  200: 'American League West',
+  201: 'American League East',
+  202: 'American League Central',
+  203: 'National League West',
+  204: 'National League East',
+  205: 'National League Central'
+}
+
+export const ciTypes: { [clinchIndicator: string]: string } = {
+  w: 'wild card',
+  y: 'division champ',
+  z: 'best record in league'
+}
