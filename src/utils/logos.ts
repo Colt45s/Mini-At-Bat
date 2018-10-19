b5f3a2961a1ed8726db9bd9c8b2295fbb32ba7ed
Colt45s <kzk0693@gmail.com>
2018-10-20 03:54:23 +0900

    add team logos

---
+++ src/utils/logos.ts
@@ -0,0 +1,65 @@
+import * as ari from 'assets/logo/ari.png'
+import * as atl from 'assets/logo/atl.png'
+import * as bal from 'assets/logo/bal.png'
+import * as bos from 'assets/logo/bos.png'
+import * as chc from 'assets/logo/chc.png'
+import * as cin from 'assets/logo/cin.png'
+import * as cle from 'assets/logo/cle.png'
+import * as col from 'assets/logo/col.png'
+import * as cws from 'assets/logo/cws.png'
+import * as det from 'assets/logo/det.png'
+import * as hou from 'assets/logo/hou.png'
+import * as kc from 'assets/logo/kc.png'
+import * as laa from 'assets/logo/laa.png'
+import * as lad from 'assets/logo/lad.png'
+import * as mia from 'assets/logo/mia.png'
+import * as mil from 'assets/logo/mil.png'
+import * as min from 'assets/logo/min.png'
+import * as nym from 'assets/logo/nym.png'
+import * as nyy from 'assets/logo/nyy.png'
+import * as oak from 'assets/logo/oak.png'
+import * as phi from 'assets/logo/phi.png'
+import * as pit from 'assets/logo/pit.png'
+import * as sd from 'assets/logo/sd.png'
+import * as sea from 'assets/logo/sea.png'
+import * as sf from 'assets/logo/sf.png'
+import * as stl from 'assets/logo/stl.png'
+import * as tb from 'assets/logo/tb.png'
+import * as tex from 'assets/logo/tex.png'
+import * as tor from 'assets/logo/tor.png'
+import * as wsh from 'assets/logo/wsh.png'
+
+export const teamLogos: {
+  [id: number]: any
+} = {
+  109: ari,
+  144: atl,
+  110: bal,
+  111: bos,
+  112: chc,
+  113: cin,
+  114: cle,
+  115: col,
+  145: cws,
+  116: det,
+  117: hou,
+  118: kc,
+  108: laa,
+  119: lad,
+  146: mia,
+  158: mil,
+  142: min,
+  121: nym,
+  147: nyy,
+  133: oak,
+  143: phi,
+  134: pit,
+  135: sd,
+  136: sea,
+  137: sf,
+  138: stl,
+  139: tb,
+  140: tex,
+  141: tor,
+  120: wsh
+}
