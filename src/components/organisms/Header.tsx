5f6b87c4a1abc8d9df04d6731746147afab9ad6b
Colt45s <kzk0693@gmail.com>
2018-10-29 23:18:56 +0900

    change link add query string

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { Container, Menu } from 'semantic-ui-react'
 
-const PageHeader = () => (
+const PageHeader = (props: any) => (
   <Menu borderless={true} fixed="top" size="large" inverted={true}>
     <Container>
       <Menu.Item header={true}>Mini-At-Bat</Menu.Item>
       <Menu.Menu position="right">
-        <Menu.Item as={Link} to="/standings">
+        <Menu.Item
+          as={Link}
+          to={{ pathname: '/standings', search: props.location.search }}
+        >
           Standings
         </Menu.Item>
-        <Menu.Item as={Link} to="/schedule">
+        <Menu.Item
+          as={Link}
+          to={{ pathname: '/schedule', search: props.location.search }}
+        >
           Schedules
         </Menu.Item>
-        <Menu.Item as={Link} to="/stats">
+        <Menu.Item
+          as={Link}
+          to={{ pathname: '/stats', search: props.location.search }}
+        >
           Stats
         </Menu.Item>
-        <Menu.Item as={Link} to="/draft">
+        <Menu.Item
+          as={Link}
+          to={{ pathname: '/draft', search: props.location.search }}
+        >
           Draft
         </Menu.Item>
       </Menu.Menu>
@@ -24,4 +37,4 @@ const PageHeader = () => (
   </Menu>
 )
 
-export default PageHeader
+export default withRouter(PageHeader)
