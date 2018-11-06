8b92c78ef967a9f9f6bdaccacd5be3c3e4057a58
Colt45s <kzk0693@gmail.com>
2018-11-07 01:42:35 +0900

    fix

--- src/components/templates/PageTemplate.tsx
+++ src/components/templates/PageTemplate.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import MainContainer from '../atoms/MainContainer'
-import FixHeader from '../organisms/FixableHeader'
+import FixableHeader from '../organisms/FixableHeader'
 import Header from '../organisms/Header'
 
 const PageTemplate = ({ children }: any) => {
   return (
     <>
       <Header />
-      <FixHeader />
+      <FixableHeader />
       <MainContainer>{children}</MainContainer>
     </>
   )
